0b3144d453daad7f6f3765d2b0ab3888d2ec9a50
NH <nicki.hysterics@gmail.com>
2024-11-30 02:28:02 +0500

    Task7

--- Tyuiu.MusinND.Sprint5.Task7.V9.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint5.Task7.V9.Lib/DataService.cs
@@ -4,6 +4,22 @@ namespace Tyuiu.MusinND.Sprint5.Task7.V9.Lib
 {
     public class DataService : ISprint5Task7V9
     {
+        public string LoadDataAndSave(string path)
+        {
+            // Читаем весь текст из файла
+            string fileContent = File.ReadAllText(path);
 
+            // Фильтруем только те символы, которые не являются заглавными латинскими буквами
+            var filteredContent = new string(fileContent.Where(c => !(c >= 'A' && c <= 'Z')).ToArray());
+
+            // Создаем временный файл с результатом
+            string tempFilePath = Path.GetTempFileName();
+
+            // Записываем отфильтрованный контент в новый файл
+            File.WriteAllText(tempFilePath, filteredContent);
+
+            // Возвращаем путь к новому файлу
+            return tempFilePath;
+        }
     }
 }
