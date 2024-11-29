ea0ff05d70830b986f32433206e6462b12a1174e
NH <nicki.hysterics@gmail.com>
2024-11-30 01:44:31 +0500

    Task3

--- Tyuiu.MusinND.Sprint5.Task3.V18.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint5.Task3.V18.Lib/DataService.cs
@@ -4,6 +4,28 @@ namespace Tyuiu.MusinND.Sprint5.Task3.V18.Lib
 {
     public class DataService : ISprint5Task3V18
     {
+        public string SaveToFileTextData(int x)
+        {
+            // Выражение F(x) = 2.12 * x^3 + 1.05 * x^2 + 4.1 * x * 2
+            double result = 2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x * 2;
 
+            // Округляем результат до 3 знаков после запятой
+            result = Math.Round(result, 3);
+
+            // Генерируем путь к временному файлу
+            string filePath = Path.GetTempFileName();
+
+            // Записываем результат в бинарный файл
+            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            {
+                writer.Write(result);
+            }
+
+            // Выводим результат на консоль
+            Console.WriteLine($"Результат вычисления F({x}) = {result}");
+
+            // Возвращаем путь к файлу
+            return result.ToString();
+        }
     }
 }
