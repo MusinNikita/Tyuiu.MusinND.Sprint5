c0426b97ce00eb5a781ebd808fc64b52b1fc073b
NH <nicki.hysterics@gmail.com>
2024-11-29 15:41:46 +0500

    Task2

--- Tyuiu.MusinND.Sprint5.Task2.V25.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint5.Task2.V25.Lib/DataService.cs
@@ -4,8 +4,8 @@ namespace Tyuiu.MusinND.Sprint5.Task2.V25.Lib
 {
     public class DataService : ISprint5Task2V25
     {
-        // Метод для замены нечетных чисел на 0
-        public void ReplaceOddNumbersWithZero(ref int[,] matrix)
+        // Метод для замены нечетных чисел на 0 и возвращения измененной матрицы
+        public int[,] ReplaceOddNumbersWithZero(int[,] matrix)
         {
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
@@ -17,6 +17,7 @@ namespace Tyuiu.MusinND.Sprint5.Task2.V25.Lib
                     }
                 }
             }
+            return matrix; // Возвращаем измененную матрицу
         }
 
         // Метод для сохранения двумерного массива в CSV файл
@@ -24,6 +25,8 @@ namespace Tyuiu.MusinND.Sprint5.Task2.V25.Lib
         {
             // Создаем временный файл
             string filePath = Path.GetTempFileName();
+
+            // Используем StreamWriter для записи в файл
             using (StreamWriter sw = new StreamWriter(filePath))
             {
                 // Записываем в файл результат
@@ -45,19 +48,6 @@ namespace Tyuiu.MusinND.Sprint5.Task2.V25.Lib
 
             // Возвращаем путь к созданному файлу
             return filePath;
-        }
-
-        // Метод для вывода двумерного массива на консоль
-        public void PrintMatrix(int[,] matrix)
-        {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
         }
     }
 }
