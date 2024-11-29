1ae1a8763aba87c6c2c99ed00df38dd3e7847ba5
NH <nicki.hysterics@gmail.com>
2024-11-30 02:06:58 +0500

    Task4

--- Tyuiu.MusinND.Sprint5.Task4.V7.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint5.Task4.V7.Lib/DataService.cs
@@ -1,4 +1,6 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.MusinND.Sprint5.Task4.V7.Lib
 {
@@ -6,28 +8,15 @@ namespace Tyuiu.MusinND.Sprint5.Task4.V7.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            try
-            {
-                // Читаем вещественное число из файла
-                string fileContent = File.ReadAllText(path);
-                if (!double.TryParse(fileContent, out double x))
-                {
-                    throw new FormatException("Ошибка: Содержимое файла не является корректным числом.");
-                }
+            // Читаем вещественное число из файла
+            string fileContent = File.ReadAllText(path);
+            double x = double.Parse(fileContent); // Предполагаем, что данные корректны
 
-                // Вычисляем y = (1 / cos(x)) + x^3
-                double y = (1 / Math.Cos(x)) + Math.Pow(x, 3);
+            // Вычисляем y = (1 / cos(x)) + x^3
+            double y = (1 / Math.Cos(x)) + Math.Pow(x, 3);
 
-                // Округляем результат до 3 знаков после запятой
-                y = Math.Round(y, 3);
-
-                // Возвращаем результат
-                return y;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException($"Ошибка при обработке файла: {ex.Message}", ex);
-            }
+            // Округляем результат до 3 знаков после запятой
+            return Math.Round(y, 3);
         }
     }
 }
